5eb645065a8d666b892c48fa1716aa2e9db8e416
La Quốc Thắng <quocthang0507@gmail.com>
2021-02-06 19:47:09 +0700

    Update form

--- TicTacToe/GUI/MainForm.Designer.cs
+++ TicTacToe/GUI/MainForm.Designer.cs
@@ -34,9 +34,10 @@ namespace TicTacToe
 			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
 			this.Text = "Tic Tac Toe";
